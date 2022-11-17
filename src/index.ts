501830458dd08593a7373d1d1f2874104bd09c71
Charles Crossan <crossan007@gmail.com>
2022-11-17 10:56:11 -0500

    refactor!: export individual methods instead of class
    feature: more filters; readmeio

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Block, parse as CommentParser } from "comment-parser";
-import axios from "axios";
+import axios, { AxiosHeaders, AxiosResponse } from "axios";
 import crypto from "crypto";
 import VinylFS from "vinyl-fs";
 import File from "vinyl";
 import fs from "fs";
+import { decode } from 'html-entities';
 
 /**
  * Applies any number of transformations to the Axios response content
@@ -25,11 +26,26 @@ export type ExternalDocumentRecord = {
   Matches: boolean;
 };
 
+type URLCacheRecord = {
+  filtersApplied: string[];
+  filteredContent: string;
+  hash: string;
+  isCacheRecord: boolean;
+  isError?: boolean;
+};
+
+function isURLCacheRecord(o: any): o is URLCacheRecord {
+  return (o as URLCacheRecord).isCacheRecord == true;
+}
+
 const TS_DOC_TAG_NAME = "ExternalDocSource";
+const TS_DOC_CACHE_DIR = ".tsdoc-link-cache";
+const USER_AGENT_HEADER = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0";
 
 let shouldUpdateFiles: boolean = true;
+let shouldCacheResponses: boolean = true;
 
-let URLCaches: Record<string, string | Promise<string>> = {};
+let URLCaches: Record<string, URLCacheRecord | Promise<URLCacheRecord>> = {};
 let allFilters: FilterFunctions = {
   body: (content: string) => {
     const bodyRegex = new RegExp(/(<body[\s\S]*?<\/body.*?>)/gim);
@@ -37,7 +53,19 @@ let allFilters: FilterFunctions = {
     if (bodyMatches == null || bodyMatches[1] == null) {
       return content;
     }
-    return content[1];
+    return bodyMatches[1];
+  },
+  nonce: (content)=>content.replace(/<.*(?:(?:nonce)|(?:csrfmiddlewaretoken)).*>/ig,""),
+  readmeio: (content)=>{
+    const r = new RegExp(/<script.*?data-initial-props="(.*?)"><\/script/,"gim")
+    const result = r.exec(content)
+    if (result == null || result[1]==null) {
+      return content
+    }
+    // ReadmeIO gives us the OAS JSON blob
+    const decoded = decode(result[1])
+    const slug = new RegExp(/"slugUpdatedAt".*?,/,"gmi")
+    return decoded.replace(slug,"")
   }
 };
 
@@ -66,56 +94,82 @@ export function SetExternalDocumentFilters(filters: FilterFunctions) {
   };
 }
 
-export async function GetURLHash(url: string, filters: string[]) {
+export async function GetURLHash(url: string, filters: string[]): Promise<URLCacheRecord> {
   if (URLCaches.hasOwnProperty(url)) {
-    if (typeof URLCaches[url] == "string") {
-      //log("using cached url hash" + url)
+    if (isURLCacheRecord(URLCaches[url])) {
       return URLCaches[url];
-    } else if (typeof URLCaches[url].hasOwnProperty("then")) {
-      //log("waiting for cached url hash promise"+ url)
-      return await URLCaches[url];
+    } else if (URLCaches[url].hasOwnProperty("then")) {
+      return await (URLCaches[url] as Promise<URLCacheRecord>);
     }
   }
 
-  const pagePromise = axios({
-    url: url
-  })
-    .then((d) => {
-      try {
-        if (typeof d.data != "string") {
-          return "Non-string returned: " + d.data;
-        }
-        let pageData = d.data;
-        let filtersApplied = [];
-        for (let f of filters) {
-          if (typeof allFilters[f] == "function") {
-            let newData = allFilters[f](pageData);
-            if (newData != pageData) {
-              filtersApplied.push(f);
-              pageData = newData;
-            }
+  URLCaches[url] = new Promise<URLCacheRecord>(async (resolve, reject) => {
+    let response: AxiosResponse;
+    let pageData: string = "";
+    let filtersApplied: string[] = [];
+  
+    try {
+      response = await axios({ 
+        headers: {
+          "User-Agent": USER_AGENT_HEADER
+        },
+        url: url 
+      });
+    } catch (urlError) {
+      return resolve({
+        filteredContent: "",
+        filtersApplied: [],
+        hash: "BAD URL: " + urlError.message,
+        isCacheRecord: true,
+        isError: true
+      });
+    }
+    try {
+      if (typeof response.data != "string") {
+        return resolve({
+          filteredContent: "",
+          filtersApplied: [],
+          hash: "Non-string returned: " + response.data,
+          isCacheRecord: true,
+          isError: true
+        });
+      }
+      pageData = response.data;
+      for (let f of filters) {
+        if (typeof allFilters[f] == "function") {
+          let newData = allFilters[f](pageData);
+          if (newData != pageData) {
+            filtersApplied.push(f);
+            pageData = newData;
           }
         }
-
-        return (
-          crypto.createHash("sha256").update(pageData).digest("hex").substring(0, 6) +
-          (filtersApplied.length > 0 ? "-" + filtersApplied.join(",") : "")
-        );
-      } catch (err) {
-        console.log("error", err);
-        return "Unable to hash page: " + err.message;
       }
-    })
-    .catch((urlError) => {
-      return "BAD URL: " + urlError.message;
+    } catch (err) {
+      return resolve({
+        filteredContent: pageData,
+        filtersApplied: filtersApplied,
+        hash: "Unable to hash page: " + err.message,
+        isCacheRecord: true,
+        isError: true
+      });
+    }
+
+    const hash =
+      crypto.createHash("sha256").update(pageData).digest("hex").substring(0, 6) +
+      (filtersApplied.length > 0 ? "-" + filtersApplied.join(",") : "");
+      return resolve({
+      filteredContent: pageData,
+      hash: hash,
+      isCacheRecord: true,
+      filtersApplied: filtersApplied
     });
-  URLCaches[url] = pagePromise;
-  //log("using new url hash promise"+ url)
-  return pagePromise;
+    //log("using new url hash promise"+ url)
+  });
+  return URLCaches[url];
 }
 
 export async function CheckGlobDocs(glob: string): Promise<ExternalDocumentRecord[]> {
-  let allDocs: Promise<ExternalDocumentRecord[]>[] = []
+  let allDocs: Promise<ExternalDocumentRecord[]>[] = [];
   return new Promise<ExternalDocumentRecord[]>((resolve, reject) => {
     VinylFS.src(glob)
       .on("data", async (d: File) => {
@@ -126,7 +180,7 @@ export async function CheckGlobDocs(glob: string): Promise<ExternalDocumentRecor
         allDocs.push(CheckFileDocs(d));
       })
       .on("end", async () => {
-        let temp = (await Promise.all(allDocs)).flatMap(x=>x)
+        let temp = (await Promise.all(allDocs)).flatMap((x) => x);
         resolve(temp);
       });
   });
@@ -160,8 +214,16 @@ export async function CheckFileDocs(file: File): Promise<ExternalDocumentRecord[
       if (pd.SavedExternalDocHash.split("-").length >= 2) {
         filters = pd.SavedExternalDocHash.split("-")[1].split(",");
       }
-      pd.CurrentExternalDocHash = await GetURLHash(pd.ExternalDocSource, filters);
-      pd.Matches = pd.SavedExternalDocHash == pd.CurrentExternalDocHash;
+      const hashRecord = await GetURLHash(pd.ExternalDocSource, filters);
+      if (shouldCacheResponses && ! hashRecord.isError) {
+        try {
+          fs.mkdirSync(`${TS_DOC_CACHE_DIR}`);
+        }
+        catch (err){}
+        fs.writeFileSync(`${TS_DOC_CACHE_DIR}/${hashRecord.hash}.html`,hashRecord.filteredContent);
+      }
+      pd.CurrentExternalDocHash = hashRecord.hash
+      pd.Matches = ! hashRecord.isError && pd.SavedExternalDocHash == pd.CurrentExternalDocHash;
       return pd;
     });
 
